4ebcd979ab5cc13a3e5c3ebdac5ed0d16d06f438
AsahiRei <asahirei123@gmail.com>
2026-09-06 09:05:44 +0800

    fix

--- data/index.ts
+++
@@ -1,5 +0,0 @@
-export { PROFILE } from "./profile";
-export { SKILLS } from "./skills";
-export { PROJECTS } from "./projects";
-export { EXPERIENCES } from "./experience";
-export { CERTIFICATES } from "./certificates";
